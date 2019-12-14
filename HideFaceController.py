8ef171b90c0b4d95acd880b5ec48cc694a20fab9
Ivan Yakovenko <ivan.yakovenko@icloud.com>
2019-12-14 13:00:09 +0300

    Change scale algorithm, fix face detection on distance

--- HideFaceController.py
+++ HideFaceController.py
@@ -6,7 +6,6 @@ from PyQt5 import Qt, QtCore, QtGui, QtWidgets
 import cv2
 import time
 import platform
-# import sys
 
 from HideFaceView import Ui_HideFaceView
 
@@ -72,13 +71,14 @@ class HideFaceApp(QtWidgets.QWidget):
             self.out.release()
             self.ui.startButton.setText("Start")
             return
-        self.DetectFaces(frame=frame)
+        
+        self.DetectFaces(frame)
         self.ShowSaveFrame(frame)
         
     def ShowSaveFrame(self, frame):
         self.out.write(frame)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        scaling_factor = 0.5
+        scaling_factor = 640 / frame.shape[1]
         frame = cv2.resize(frame, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
         height, width, channel = frame.shape
         step = channel * width
@@ -86,10 +86,9 @@ class HideFaceApp(QtWidgets.QWidget):
         self.ui.imageLabel.setPixmap(QtGui.QPixmap.fromImage(qImg))
     
     def DetectFaces(self, frame):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.resize(gray, None, fx=0.25, fy=0.25, interpolation=cv2.INTER_AREA)
+        newframe = cv2.resize(frame, None, fx=0.25, fy=0.25, interpolation=cv2.INTER_AREA)
         accuracy_val = self.ui.accurancySpinBox.value()
-        face_rects = self.face_cascade.detectMultiScale(gray, accuracy_val, 3)
+        face_rects = self.face_cascade.detectMultiScale(newframe, accuracy_val, 3)
         value = self.ui.blurSlider.value()
         value = int(( len(prime) / 100 ) * value)
         for (x, y, w, h) in face_rects:
@@ -125,16 +124,3 @@ class HideFaceApp(QtWidgets.QWidget):
             self.cap.release()
             self.out.release()
             self.ui.startButton.setText("Start")
-
-# if __name__ == "__main__":
-#     app = QtWidgets.QApplication(sys.argv)
-#     MainWindow = HideFaceApp()
-#     MainWindow.show()
-#     sys.exit(app.exec_())
-    
-    
-# FIXME
-# app crashes when video ends DONE
-# init doesnt turn off file browse button 
-# app chooses webcam by default while file button is active DONE
-#TODO
